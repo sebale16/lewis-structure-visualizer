e539d3b63ec9375ccecb2c357b5c0a9bfe26121e
spl835 <sebastian.leiman@utexas.edu>
2025-03-28 14:31:43 -0500

    Revert "initial commit"
    This reverts commit 0cc206e2b4ff4b8677d45315c9ab62d905d8dd64.

--- display/build/CMakeFiles/molecular_orbital_viewer.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for molecular_orbital_viewer.
--- display/build/CMakeFiles/molecular_orbital_viewer.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for molecular_orbital_viewer.
-# This may be replaced when dependencies are built.
