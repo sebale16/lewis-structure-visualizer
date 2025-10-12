ac79fefea278c9e74a9da0f82239dbe3d8cf6b17
Sebastian Leiman <sebastian.leiman@gmail.com>
2025-10-12 16:56:38 -0500

    more

--- display2/src/main.cpp
+++ display2/src/main.cpp
@@ -1,7 +1,12 @@
 #include <GLFW/glfw3.h>
 #include <webgpu/webgpu_cpp.h>
 
+#if defined(__EMSCRIPTEN__)
+#include <emscripten/emscripten.h>
+#endif
+
 #include <iostream>
+#include <vector>
 
 wgpu::Instance          g_instance;
 wgpu::Adapter           g_adapter;
@@ -13,13 +18,12 @@ wgpu::TextureFormat     g_format;
 
 int main() {
     static const auto kTimedWaitAny = wgpu::InstanceFeatureName::TimedWaitAny;
+    wgpu::InstanceDescriptor instance_desc = {};
+    instance_desc.nextInChain = nullptr;
+    instance_desc.requiredFeatureCount = 1;
+    instance_desc.requiredFeatures = &kTimedWaitAny;
 
-    wgpu::InstanceDescriptor desc = {};
-    desc.nextInChain = nullptr;
-    desc.requiredFeatureCount = 1;
-    desc.requiredFeatures = &kTimedWaitAny;
-
-    g_instance = wgpu::CreateInstance(&desc);
+    g_instance = wgpu::CreateInstance(&instance_desc);
 
     if (g_instance == nullptr) {
         std::cerr << "Could not initialize WebGPU!" << std::endl;
@@ -34,7 +38,7 @@ int main() {
         [](wgpu::RequestAdapterStatus status, wgpu::Adapter a, wgpu::StringView message) {
             if (status != wgpu::RequestAdapterStatus::Success) {
                 std::cout << "RequestAdapter: " << message.data << "\n";
-                exit(0);
+                exit(1);
             }
             g_adapter = std::move(a);
         }
@@ -50,6 +54,27 @@ int main() {
     std::cout << "DeviceID: " << std::hex << adapter_info.deviceID << std::dec << "\n";
     std::cout << "Name: " << adapter_info.device.data << "\n";
     std::cout << "Driver description: " << adapter_info.description.data << "\n";
+
+    wgpu::DeviceDescriptor device_desc = {};
+    device_desc.SetUncapturedErrorCallback(
+        [](const wgpu::Device&, wgpu::ErrorType error_type, wgpu::StringView message) {
+            std::cout << "Error: " << static_cast<uint64_t>(error_type) << " - message: " << message.data << "\n";
+        }
+    );
+
+    wgpu::Future device_future = g_adapter.RequestDevice(
+        &device_desc,
+        wgpu::CallbackMode::WaitAnyOnly,
+        [](wgpu::RequestDeviceStatus status, wgpu::Device d, wgpu::StringView message) {
+            if (status != wgpu::RequestDeviceStatus::Success) {
+                std::cout << "RequestDevice: " << message.data << "\n";
+                exit(1);
+            }
+            g_device = std::move(d);
+        }
+    );
+    g_instance.WaitAny(device_future, UINT64_MAX);
+
     return EXIT_SUCCESS;
 
 }
