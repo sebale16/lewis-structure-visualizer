3ece7dd67d8c2bc489b77807bc19d2c282480e7a
Sebastian Leiman <sebastian.leiman@gmail.com>
2025-12-30 03:11:15 -0600

    test molecule file (not included in CMake)

---
+++ display2/src/test_molecule.cpp
@@ -0,0 +1,14 @@
+#include "molecule.hpp"
+
+#include <iostream>
+
+int main() {
+    std::string jsonFilePath = "/home/seb/projects/lewis-structure-visualizer/solver/XeF2_0.json";
+    std::string csvFilePath = "/home/seb/projects/lewis-structure-visualizer/data/data.csv";
+
+    molecule::Molecule mol;
+    mol.FillMoleculeFromJSON(jsonFilePath, csvFilePath);
+    auto geo = static_cast<int>(mol.GetGeometry().value());
+    std::cout << geo << "\n";
+    return 0;
+}
