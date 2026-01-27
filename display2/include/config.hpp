42983fd7df0c86f5ae016b0e8fbb5f08b62ed7be
Sebastian Leiman <84030375+sebale16@users.noreply.github.com>
2026-01-27 00:27:19 -0600

    Revert "Display2"

--- display2/include/config.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#define GLM_FORCE_RADIANS
-
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/vec3.hpp>
-#include <glm/gtc/quaternion.hpp>
--- display2/test.sh
+++
@@ -1,1 +0,0 @@
-g++ -std=c++23 -I include/ src/test_molecule.cpp src/molecule.cpp -o bin/test_molecule
